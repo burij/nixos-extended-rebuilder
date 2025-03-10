9fb448eb6a6a32588a4c83df079f1754c4aba7d0
burij <michael.burij@gmail.com>
2025-03-10 20:27:57 +0100

    made usage of need

--- app.lua
+++ app.lua
@@ -1,15 +1,18 @@
-dofile "env.lua"
-local utils = require "modules.utils"
+need = require "need"
+local core = need "lua-light-wings" core.globalize(core)
+local utils = need "utils"
+local conf = utils.get_configuration("./example-config.lua")
+if conf.debug_mode then dofile("tests.lua") end
+
+--------------------------------------------------------------------------------
 
 local function application()
-    local conf = utils.get_configuration("./example-config.lua")
     msg("nixos extended rebuilder is cooking your config...")
     if conf.debug_mode then
         msg("content of the loaded configuration:")
         msg(conf)
     end
 end
---------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
 application()
