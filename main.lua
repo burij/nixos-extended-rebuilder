f52d1bb4693fc648a2e5cd9e4afb4262fb4aaaad
burij <michael.burij@gmail.com>
2025-03-18 19:40:04 +0100

    uncomplete function to read arguments

--- main.lua
+++ main.lua
@@ -3,20 +3,34 @@ local core = need "lua-light-wings" core.globalize(core)
 local utils = need "utils"
 local conf = utils.get_configuration("conf.lua")
 if conf.debug_mode then dofile("tests.lua") end
+local M = {}
 --------------------------------------------------------------------------------
 
 local function application()
     msg("nixos extended rebuilder is cooking your config...")
+
     if conf.debug_mode then
         msg("content of the loaded configuration:")
         msg(conf)
     end
 
-    msg(arg[1])
+    M.arg_present(arg, "rebuild")
 
 end
 
 --------------------------------------------------------------------------------
+
+function M.arg_present(x, y)
+-- TODO: returns true, if value y is present in table x
+    is_dictionary(x)
+    is_string(y)
+    map(x, msg)
+    local output = true
+    if conf.debug_mode then msg(output) end
+    return is_boolean(output)
+end
+
+
 conf.upgrade = [[
 echo "NixOS update..."
 sudo nixos-rebuild switch --upgrade
