9fc9c8ca5ec46c251d44c5f9bb8bfb4c023b12bd
burij <michael.burij@gmail.com>
2025-06-09 23:21:58 +0200

    third attempt to fix dconf bug

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local M = {} _G.need = require "need"
-local version = "NixOS extended rebuilder, Version 0.9.1"
+local version = "NixOS extended rebuilder, Version 0.9.2"
 
 local core = require "modules.lua-light-wings" core.globalize(core)
 local utils = require "modules.utils"
